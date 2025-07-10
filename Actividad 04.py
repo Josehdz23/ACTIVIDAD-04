4e6284c219aea12a5371895a71521e0e47e43ab8
Josehdz23 <jose.jahr.2016@gmail.com>
2025-07-09 19:23:53 -0600

    Agrego de funciones para iniciar el programa, por segunda vez xd

--- Actividad 04.py
+++ Actividad 04.py
@@ -6,25 +6,28 @@ def menu():
     print("- - - - CLINICA - - - -\n1. Agregar paciente\n2. Atender pacientes\n3. Mostrar pacientes\n4. Agregar Medicamentos\n5. Entregar Medicamentos\n6. Mostrar medicamentos\n7. Salir")
 
 def main():
-    try:
-        op = int(input("Selecciona una opcion: "))
-        match op:
-            case 1:
-                ingreso()
-            case 2:
-                atender()
-            case 3:
-                mostrar_cola()
-            case 4:
-                agregar_Medicamentos()
-            case 5:
-                entregar_Medicamentos()
-            case 6:
-                mostrar_Medicamentos()
-            case 7:
-                print("Saliendo...")
-            case _:
-                print("Esa opción no existe!!")
-    except Exception as ex:
-        print(ex)
+    menu()
+    while True:
+        try:
+            op = int(input("Selecciona una opcion: "))
+            match op:
+                case 1:
+                    ingreso()
+                case 2:
+                    atender()
+                case 3:
+                    mostrar_cola()
+                case 4:
+                    agregar_Medicamentos()
+                case 5:
+                    entregar_Medicamentos()
+                case 6:
+                    mostrar_Medicamentos()
+                case 7:
+                    print("Saliendo...")
+                case _:
+                    print("Esa opción no existe!!")
+        except Exception as ex:
+            print(ex)
 
+main()
