bc5099dd80a90c3ffb7e70f275784712cbe7070b
Josehdz23 <jose.jahr.2016@gmail.com>
2025-07-09 19:04:57 -0600

    funcion mostrar medicamentos agregada

--- farmacia.py
+++ farmacia.py
@@ -9,5 +9,14 @@ def agregar_Medicamentos():
         else:
             print("Debe de ingresar un medicamento!!!")
 
+def entregar_Medicamentos():
+    print(f"Se entregara el medicamento {medicamentos.pop()}")
+
+def mostrar_Medicamentos():
+    for i in medicamentos:
+        cont = 1
+        print(f"Medicamento {cont}: {i}")
+        cont = cont + 1
+
 
 
