505d3f823f0ea1ffbf8973f6f15e3f41387b2c0a
Josehdz23 <jose.jahr.2016@gmail.com>
2025-07-09 19:22:03 -0600

    Agrego de funciones para iniciar el programa

--- Actividad 04.py
+++ Actividad 04.py
@@ -1,4 +1,30 @@
 #Clinica medica
+from farmacia import agregar_Medicamentos, entregar_Medicamentos, mostrar_Medicamentos
+from recepcion import ingreso, mostrar_cola, atender
 
 def menu():
     print("- - - - CLINICA - - - -\n1. Agregar paciente\n2. Atender pacientes\n3. Mostrar pacientes\n4. Agregar Medicamentos\n5. Entregar Medicamentos\n6. Mostrar medicamentos\n7. Salir")
+
+def main():
+    try:
+        op = int(input("Selecciona una opcion: "))
+        match op:
+            case 1:
+                ingreso()
+            case 2:
+                atender()
+            case 3:
+                mostrar_cola()
+            case 4:
+                agregar_Medicamentos()
+            case 5:
+                entregar_Medicamentos()
+            case 6:
+                mostrar_Medicamentos()
+            case 7:
+                print("Saliendo...")
+            case _:
+                print("Esa opción no existe!!")
+    except Exception as ex:
+        print(ex)
+
