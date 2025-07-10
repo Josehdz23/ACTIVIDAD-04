62bbdcd8cd7dd84e4aa5b2bb8a6a65b20eb84486
AnderSeven <andersonmanoloalvaradoalvarez@gmail.com>
2025-07-09 18:57:08 -0600

    cambios

--- recepcion.py
+++ recepcion.py
@@ -1,7 +1,14 @@
 from collections import deque
-def recepcion():
-    nombre = print("Ingrese su nombre: ")
+def ingreso():
+    nombre = input("Ingrese su nombre: ")
     cola = deque()
     cola.append(nombre)
+    print("Se ha registrado con exito")
+
+def atender():
+    a = cola.popleft()
+    print(f"Se ha atendido a {a}")
+
+def mostrar_cola():
     for i in cola:
         print(i)
